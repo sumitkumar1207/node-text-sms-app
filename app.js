06218e3c8f8eca6917d0aa67089446406cf3faac
sumitkumar1207 <logsumit28@gmail.com>
2018-10-23 11:58:00 +0530

    Done with the UI part and setting up the server.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+//Initialize the app
+const app = express();
+
+//Setting up template engine
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
+
+//Setting up public folder
+app.use(express.static(__dirname + "/public"));
+
+//BodyParser middlewarwe
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Defining the port
+const port = 3000;
+
+//Connect to the server
+const server = app.listen(port, () =>
+  console.log(`Server started on the port ${port}`)
+);
